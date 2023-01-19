0c7707d889c14bf55500c6d5e1d3d3095b78f7ad
muq2 <muqadam.sabir@student.allenisd.org>
2023-01-19 16:28:32 -0600

    Try this

---
+++ .build/x86_64-unknown-linux-gnu/debug/CBase32.build/cbase32.c.d
@@ -0,0 +1,24 @@
+dependencies: \
+  /home/muqadam-sabir/ModelScheduler/Sources/CBase32/cbase32.c \
+  /home/muqadam-sabir/ModelScheduler/Sources/CBase32/include/module.modulemap \
+  /usr/include/string.h \
+  /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+  /usr/include/features.h /usr/include/features-time64.h \
+  /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+  /usr/include/x86_64-linux-gnu/bits/timesize.h \
+  /usr/include/stdc-predef.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+  /usr/include/x86_64-linux-gnu/bits/long-double.h \
+  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+  /usr/lib/clang/13.0.0/include/stddef.h \
+  /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+  /usr/include/strings.h \
+  /home/muqadam-sabir/ModelScheduler/Sources/CBase32/cbase32.h \
+  /usr/lib/clang/13.0.0/include/stdint.h /usr/include/stdint.h \
+  /usr/include/x86_64-linux-gnu/bits/types.h \
+  /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+  /usr/include/x86_64-linux-gnu/bits/time64.h \
+  /usr/include/x86_64-linux-gnu/bits/wchar.h \
+  /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+  /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h
