5adfe4143b9e822c5ca97c4bbe87c966e36251c0
captainbboy <bkapla01@gmail.com>
2023-02-07 17:08:04 -0600

    auto get classes from ./classes/data

--- Public/js/classes.js
+++ Public/js/classes.js
@@ -42,19 +42,50 @@ function removeCourse(id) {
 
 }
 
-function getCoursesFromServer()  {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "./classes/data");
-    xhr.send();
-    xhr.responseType = "json";
-    xhr.onload = () => {
-	if (xhr.readyState == 4 && xhr.status == 200) {
-	    const data = xhr.response;
-	    console.log(data)
-	} else {
-	    console.log(`Error: ${xhr.status}`);
-	}
-    }
+async function getCoursesFromServer() {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", "./classes/data");
+        xhr.send();
+        xhr.responseType = "json";
+        xhr.onload = () => {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                const data = xhr.response;
+                console.log(data)
+                resolve(data);
+            } else {
+                console.log(`Error: ${xhr.status}`);
+                reject(xhr.status);
+            }
+        }
+    });
 }
 
-getCoursesFromServer();
+document.addEventListener("DOMContentLoaded", async function () {
+    const availableCourses = document.getElementById("availableCourses");
+
+    const courses = await getCoursesFromServer();
+
+    console.log(courses);
+
+    // if got the courses from the server without error
+    if (courses != null) {
+        for (let i = 0; i < courses?.items?.length; i++) {
+            const course = courses.items[i];
+            console.log(course);
+
+            const courseItem = document.createElement("li");
+            courseItem.appendChild(document.createTextNode(course.name));
+
+            const addButton = document.createElement("button");
+            addButton.classList.add("addButton");
+            addButton.onclick = () => addCourse(course.code);
+            addButton.appendChild(document.createTextNode("Add"));
+
+            courseItem.appendChild(addButton);
+            courseItem.id = course.code;
+            availableCourses.appendChild(courseItem);
+        }
+    }
+
+});
