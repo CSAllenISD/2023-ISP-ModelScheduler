6d921f325db3f6fa81e189a3f23064780aaea999
Reynard G <reynard.ean.gunawan@gmail.com>
2023-02-11 00:56:10 -0600

    Finished schedule selector

--- Public/js/scheduler.js
+++ Public/js/scheduler.js
@@ -1,3 +1,27 @@
+function updateClassSchedule() {
+    const schedule = JSON.parse(window.localStorage.getItem('courses')) ?? {};
+    const classList = document.querySelectorAll('.class');
+
+    for (let i = 0; i < classList.length; i++) {
+	const span = document.getElementById(classList[i].id).getElementsByTagName("span")[0];
+
+	if (!Object.keys(schedule).some(course => classList[i].id === course)) {
+	    span.innerHTML = "Empty";
+	} else {
+	    span.innerHTML = schedule[classList[i].id];
+	}
+    }
+}
+
+const removeDuplicatesFromObject = (obj, variable) => {
+    for (let key in obj) {
+	if (obj[key] === variable) {
+	    delete obj[key];
+	}
+    }
+    return obj;
+};
+
 const classes = document.querySelectorAll('.class');
 
 for (let i = 0; i < classes.length; i++) {
@@ -9,6 +33,8 @@ for (let i = 0; i < classes.length; i++) {
 
     classPeriod.appendChild(span);
 }
+
+updateClassSchedule();
 
 const courses = [
     {
@@ -58,12 +84,21 @@ courseSelect.addEventListener('click', function (event) {
 });
 
 scheduleViewer.addEventListener('click', function (event) {
+    // Check if click target is valid div
     if (classKeyRegExp.test(event.target.id) === true || selectedCourse === null || event.target.id === "scheduleViewer" || event.target.id === "") return;
-    console.log("Valid class div selected");
 
-    // Query selected class into db
+    // Only allow 1 class in 1 slot
+    var currentSchedule = JSON.parse(window.localStorage.getItem("courses")) ?? {};
+    currentSchedule = removeDuplicatesFromObject(currentSchedule, selectedCourse);
 
-    // Update course to reflect updated class schedule (grab all classes in db then update schedule)
+    // Update JSON of schedule
+    currentSchedule[event.target.id] = selectedCourse;
+    window.localStorage.setItem("courses", JSON.stringify(currentSchedule));
+    document.getElementById(selectedCourse).classList.remove('selected');
+    selectedCourse = null;
+
+    // Update scheduleViewer with new schedule
+    updateClassSchedule();
 });
 
 const semesterSelecters = document.querySelectorAll('.semesterBtn');
