30e2b4cbc0e416002e392a82d6fb957ea582a8ce
TahmidR2456 <tahmidr05@gmail.com>
2023-02-15 23:47:10 -0600

    darkmode/light mode button functionality added

--- Public/js/final.js
+++ Public/js/final.js
@@ -4,3 +4,45 @@ function updateClassSchedule() {
 }
 
 updateClassSchedule()
+//dark mode and light mode feature
+document.addEventListener("DOMContentLoaded", function() { //Waits for HTML DOM content to load
+    const dmButton = document.getElementById("darkmodeButton"); //Gets darkMode button id
+
+
+    if (dmButton) { //Checks whether or not button was clicked for debug purposes
+	dmButton.addEventListener("click", function() {
+	    console.log("Button was clicked!");
+	});
+    } else {
+	console.error("Element with id 'darkMode' not found");
+    }
+
+    dmButton.addEventListener("click", dmSwitch);
+    dmButton.addEventListener("click", toggleDmButton);
+});
+
+function dmSwitch() {
+    var background = document.body;
+    var boxes = document.querySelectorAll(".grayBox");
+    background.classList.toggle("dark-mode");
+    boxes.forEach(function(box) {
+	box.classList.toggle("dm-grayBox");
+    });
+}
+
+function toggleDmButton() {
+    let dmButton = document.getElementById("darkmode");
+    if (dmButton.getAttribute("src") === "./images/sun.png") {
+	dmButton.setAttribute("src", "./images/moon.png");
+    } else {
+	dmButton.setAttribute("src", "./images/sun.png");
+    }
+}
+
+function darkMode() {
+    var dmButton = document.createElement("button");
+    dmButton.id = "darkMode";
+    dmButton.innerHTML = "Toggle Dark Mode";
+    document.body.appendChild(dmButton);
+    document.getElementById("darkmode").src="./images/moon.png";
+}
