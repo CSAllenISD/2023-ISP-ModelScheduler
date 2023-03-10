f269d24a0c32b9fd49462ae7bc2b68c2e8ba9fba
dylan-pettijohn <nalyd1369@gmail.com>
2023-03-09 20:48:26 -0600

    fixed scheduling to work with backend

--- Public/js/util.js
+++ Public/js/util.js
@@ -5,6 +5,9 @@ function combineCourses(courses) {
     //loop through all objects from database
     for(i=0; i < courses?.items?.length; i++) {
 	var course = courses?.items[i];
+	if (course.name.toLowerCase().includes("ap-gt english iv")) {
+	    console.log(course);
+	}
 	var periods = [];
 	
 	if (!codesDone.includes(course.code)) {
